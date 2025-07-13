baa1ad09eb5248610d6429d15248b256d09c9b57
Emmanuel Onyango <emmanuelonyango238@gmail.com>
2025-07-13 14:37:21 +0300

    Better message or fix

--- Backend/src/com/gymnation/Main.java
+++ Backend/src/com/gymnation/Main.java
@@ -4,15 +4,12 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Optional: use the system look & feel for a native look
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception ignored) {}
 
-        // Launch the login screen
         SwingUtilities.invokeLater(() -> {
             new Login().setVisible(true);
         });
-        
     }
 }
